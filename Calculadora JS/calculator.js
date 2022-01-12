0acd2b2d800fc6dd4b5b8851ea8a8ff9ccfed3fd
Claudio <bsilva.claudio@gmail.com>
2022-01-12 19:57:02 -0300

    edit: loop improved

--- Calculadora JS/calculator.js
+++ Calculadora JS/calculator.js
@@ -3,22 +3,32 @@ const readlineSync = require('readline-sync');
 
 function calculator(){
   console.log("\t C A L C U L A T O R");
-  let firstNumber = 0;      
+  let firstNumber, secondNumber = 0; 
+
   do{
-    let operation = readlineSync.question('Choose an operation: \n \t [+] [-] [*] [/] \n\t Press E to exit \n');
-    firstNumber = parseFloat(readlineSync.question('\nInsert the first number: '));
-    let secondNumber = parseFloat(readlineSync.question('\nInsert the second number: '));
-    if(operation == '+'){
-      op.addNumber(firstNumber,secondNumber);
-    }else if(operation == '-'){
-      op.subNumber(firstNumber,secondNumber);
-    }else if(operation == '*'){
-      op.multNumber(firstNumber,secondNumber);
-    }else if(operation == '/'){
-      op.divNumber(firstNumber,secondNumber);
+    var operation = readlineSync.question('Choose an operation: \n \t [+] [-] [*] [/] \n\n\t Press E to exit \n');
+
+    if((operation === "E") || (operation === "e")){
+      console.log('\n Leaving Calculator. .  .\n');
+      var exit = 0; // *Is boolean applicable? **why let doenst work
+      
+    }else{
+      firstNumber = parseFloat(readlineSync.question('\nInsert the first number: '));
+      secondNumber = parseFloat(readlineSync.question('\nInsert the second number: '));
+      if(operation == '+'){
+        op.addNumber(firstNumber,secondNumber);
+      }else if(operation == '-'){
+        op.subNumber(firstNumber,secondNumber);
+      }else if(operation == '*'){
+        op.multNumber(firstNumber,secondNumber);
+      }else if(operation == '/'){
+        op.divNumber(firstNumber,secondNumber);
+      }else{
+        console.log("\nInvalid opertation!\n")
+      }
     }
-  } while((firstNumber||operation||secondNumber) !== ('E'||'e'));  
-   
+  } while(exit!== 0); 
+
 }
 
 calculator();
--- Calculadora JS/src/functions.js
+++ Calculadora JS/src/functions.js
@@ -1,20 +1,20 @@
 module.exports = {
   addNumber (number, number2) {
     const result = number + number2;
-    return console.log(`Result: ${result}\n`);
+    return console.log(`\nResult: ${result}\n`);
   },
   subNumber (number, number2) {
     const result = number - number2;
-    return console.log(`Result: ${result}\n`);
+    return console.log(`\nResult: ${result}\n`);
   },
   multNumber (number, number2) {
     const result = number * number2;
-    return console.log(`Result: ${result}\n`);
+    return console.log(`\nResult: ${result}\n`);
   },
   divNumber (number, number2) {
     if (number2 > 0) {
       const result = number / number2;
-      return console.log(`Result: ${result}\n`)
+      return console.log(`\nResult: ${result}\n`)
     }else{
       throw new Error("I cant do a division by zero. :( ");      
     }
